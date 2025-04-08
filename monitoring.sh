0462a67d80e273e095f2ea8a5f1e1abaea138fec
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2025-04-08 16:44:04 +0000

    v 0.0.02

--- monitoring.sh
+++ monitoring.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 
-# CPU
+# Get information about architecture
+arc=$(uname -a)
 
-# RAM
+# 1.CPU
+# Count of Physical CPU processes
+p_cpu=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)
+# Count of Virtual CPU cores
+v_cpu=$(grep "^processor" /proc/cpuinfo | wc -l)
 
-# MEMORY
+# 2.RAM
+# Total RAM in megabytes
+t_ram=$(free -m | awk '$1 == "Mem:" {print $2}')
+# Used RAM in megabytes
+u_ram=$(free -m | awk '$1 == "Mem:" {print $3}')
+# Percentage of RAM used
+p_ram=$(free | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
 
-# CPU LOAD
+# 3.DISK
+# Calculate total disks size on megabytes without /boot
+f_dsk=$(df -m | grep "^/dev/" | grep -v "/boot$" | awk '{disk_t += $2} END {printf("%.1fGb", disk_t/1024)}')
+# Calculate how much disk space used
+u_dsk=$(df -BM | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} END {print ut}')
+# Percentage of disk space used
+p_dsk=$(df -BM | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} {ft+= $2} END {printf("%d"), ut/ft*100}')
+
+# 4.CPU load data
+# Calculates CPU load: user + system
+cpul=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
+
 
 # BOOT
 
